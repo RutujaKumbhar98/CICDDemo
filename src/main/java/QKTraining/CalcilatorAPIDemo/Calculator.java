e17a95754fbc9893fd82a94971dd70171354c051
Rutuja Kumbhar <rutujakumbhar98@gmail.com>
2021-05-15 10:14:10 +0530

    Added Addition function

---
+++ src/main/java/QKTraining/CalcilatorAPIDemo/Calculator.java
@@ -0,0 +1,11 @@
+package QKTraining.CalcilatorAPIDemo;
+
+public class Calculator 
+{
+		public int Addition(int num1,int num2)
+		{
+			int res=num1+num2;
+			return res;
+		}
+
+}
---
+++ src/test/java/QKTraining/CalcilatorAPIDemo/AdditionTestCases.java
@@ -0,0 +1,67 @@
+package QKTraining.CalcilatorAPIDemo;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+//import junit.framework.Assert;
+
+public class AdditionTestCases 
+{
+	Calculator obj;
+	int result;
+	
+	@BeforeGroups("RegressionTest")
+	public void InitGroup()
+	{
+		 System.out.println("I am in Before Group");
+		 obj=new Calculator();
+	}	
+	
+	@BeforeClass
+	public void Init()
+	{
+		 System.out.println("I am in Before class");
+		 obj=new Calculator();
+	}
+	
+	@BeforeMethod
+	public void ReInitialiseResultVar()
+	{
+		System.out.println("I am in Before Method");
+		result=0;
+	}
+	
+	@Test(priority=1,groups= {"RegressionTest"})
+	public void TestAdditionWithPositiveNumbers()
+	{
+
+		System.out.println("First test case");
+	    result=obj.Addition(10,20);
+		Assert.assertEquals(result, 30,"Addiion does not work with positive numbers");	
+	}
+	@Test(priority=2,groups= {"RegressionTest"})
+	public void TestAdditionWithZeros()
+	{
+		System.out.println("Second Test Case");
+	    result=obj.Addition(0,0);
+		Assert.assertEquals(result, 0,"Addiion does not work with Zeros");	
+	}
+	@Test(priority=3)
+	public void TestAdditionWithOnePositiveOneNegative()
+	{
+		System.out.println("Third Test case");
+	    result=obj.Addition(10, -8);
+		Assert.assertEquals(result, 2,"Addition does not work with 1 positive and 1 negative number");
+	}
+	
+	@AfterClass
+	public void TearDown()
+	{
+		System.out.println("I am in after class");
+		obj=null;
+		
+	}
+
+}
